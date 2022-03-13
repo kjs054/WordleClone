4322f5d172036ca2f9459155c9c423453363377d
Wandr Inc <kjs054111@gmail.com>
2022-03-13 14:12:13 -0400

    Simple interface finished

--- src/index.js
+++ src/index.js
@@ -1,17 +1,109 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const WORD_LENGTH = 5;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+
+  const guess = useGuess();
+
+  return (
+    <div id="container">
+      <div id="header">
+        <h1>Wordle Max</h1>
+      </div>
+      <div id="gameBoard">
+        <WordRow></WordRow>
+        <WordRow></WordRow>
+        <WordRow></WordRow>
+        <WordRow></WordRow>
+        <WordRow></WordRow>
+        <WordRow></WordRow>
+      </div>
+      <KeyBoard></KeyBoard>
+    </div>
+  );
+}
+
+const LetterBox = () => {
+  return (
+    <div class="letterbox">
+      
+    </div>
+  );
+}
+
+const WordRow = () => {
+  return (
+    <div class="wordRow">
+      <LetterBox></LetterBox>
+      <LetterBox></LetterBox>
+      <LetterBox></LetterBox>
+      <LetterBox></LetterBox>
+      <LetterBox></LetterBox>
+    </div>
+  )
+}
+
+const KeyBoard = () => {
+  const keys = [
+    "Q",
+    "W",
+    "E",
+    "R",
+    "T",
+    "Y",
+    "U",
+    "I",
+    "O",
+    "P",
+    "A",
+    "S",
+    "D",
+    "F",
+    "G",
+    "H",
+    "J",
+    "K",
+    "ENTER",
+    "L",
+    "Z",
+    "X",
+    "C",
+    "Backspace",
+    "V",
+    "B",
+    "N",
+    "M",
+  ]
+
+  return (<div id="keyboard">
+    {keys.map((key) => (
+      <button class="keyBox">{key}</button>
+    ))}
+  </div>)
+};
+
+function useGuess() {
+  
+  const [guess, setGuess] = useState("");
+  useEffect(() => {
+    const onKeyDown = ({ key }) => {
+      setGuess((curGuess) => {
+        if (curGuess.length == WORD_LENGTH) {
+          return curGuess
+        } else {
+          return curGuess + key
+        }
+      })
+    }
+    window.addEventListener('keydown', onKeyDown)
+
+    return () => {
+      document.removeEventListener('keydown', onkeydown)
+    }
+  }, [])  
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
